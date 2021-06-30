9ccba1237f3dbc6230a6a856f5ccfdbff8ee123c
TeXmeijin <i8858garden@gmail.com>
2021-06-30 13:27:41 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# expo-sandbox
+
+Expo(Bare Workflow)製のアプリにいろいろなライブラリや設計手法を導入して遊ぶためのリポジトリ。
---
+++ components/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
